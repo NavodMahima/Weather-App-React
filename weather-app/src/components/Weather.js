0a8d52843845b69696976528f9375c24ec957958
NavodMahima <gnmshihara@std.appsc.sab.ac.lk>
2024-08-14 13:08:27 +0530

    After Adding APIKey

--- weather-app/src/components/Weather.js
+++ weather-app/src/components/Weather.js
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 import './Weather.css';
 import search_icon from '../assets/search.png'
 import clear_icon from '../assets/clear.png'
 import cloud_icon from '../assets/cloud.png'
 import drizzle_icon from '../assets/drizzle.png'
 import rain_icon from '../assets/rain.png'
-import snow_icon from '../assets/snow.png'
+import snow_icon  from '../assets/snow.png'
 import wind_icon from '../assets/wind.png'
 import humidity_icon from '../assets/humidity.png'
 
 export default function Weather() {
+
+  const search = async (city)=>{
+    try{
+      const apiKey = process.env.REACT_APP_API_KEY;
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+
+    }catch(error){
+      console.error('Error fetching weather data:', error);
+    }
+  }
+
+  useEffect(()=>{
+    search("London");
+  },[])
+  
+
   return (
     <div className='weather'>
       <div className='search-bar'>
