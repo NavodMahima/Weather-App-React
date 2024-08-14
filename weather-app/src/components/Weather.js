1b339233a296b42f35138a4f821e2dbad1303f77
NavodMahima <gnmshihara@std.appsc.sab.ac.lk>
2024-08-15 03:03:39 +0530

    Signed-off-by: NavodMahima <gnmshihara@std.appsc.sab.ac.lk>

--- weather-app/src/components/Weather.js
+++ weather-app/src/components/Weather.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect,useRef,useState } from 'react';
 import './Weather.css';
 import search_icon from '../assets/search.png'
 import clear_icon from '../assets/clear.png'
@@ -11,15 +11,53 @@ import humidity_icon from '../assets/humidity.png'
 
 export default function Weather() {
 
+  const inputRef = useRef()
+
+  const [weatherData, setWeatherData] = useState(false);
+
+  const allIcons = {
+    "01d": clear_icon,
+    "01n": clear_icon,
+    "02d":cloud_icon,
+    "02n":cloud_icon,
+    "03d":cloud_icon,
+    "03n":cloud_icon,
+    "04d":drizzle_icon,
+    "04n":drizzle_icon,
+    "09d":rain_icon,
+    "04d":drizzle_icon,
+    "04n":drizzle_icon,
+    "09d":rain_icon,
+    "09n":rain_icon,
+    "10n":rain_icon,
+    "10d":rain_icon,
+    "13n":snow_icon,
+    "13d":snow_icon,
+  }
+
   const search = async (city)=>{
+    if(city === ""){
+      alert("Enter City Name 😁");
+      return;
+    }
     try{
       const apiKey = process.env.REACT_APP_API_KEY;
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
 
 
         const response = await fetch(url);
         const data = await response.json();
         console.log(data);
+        const icon = allIcons[data.weather[0].icon] || clear_icon;
+        setWeatherData({
+          humidity: data.main.humidity,
+          wind: data.wind.speed,
+          temperature: Math.floor(data.main.temp),
+          location: data.name,
+          icon: icon
+
+
+        })
 
     }catch(error){
       console.error('Error fetching weather data:', error);
@@ -34,29 +72,33 @@ export default function Weather() {
   return (
     <div className='weather'>
       <div className='search-bar'>
-        <input type='text' placeholder='Search'/>
-        <img src={search_icon} alt=''/>
+        <input ref={inputRef} type='text' placeholder='Search'/>
+        <img src={search_icon} alt='' onClick={()=>search(inputRef.current.value)}/>
       </div>
-      <img src={clear_icon} alt='weather-icon'/>
-      <p className='temperature'>16c</p>
-      <p className='location'>London</p>
+
+      {/* {weatherData?<>
+      </>:<></>} */}
+
+      <img src={weatherData.icon} alt='weather-icon'/>
+      <p className='temperature'>{weatherData.temperature}c</p>
+      <p className='location'>{weatherData.location}</p>
       <div className='weather-info'>
       <div className='col'>
         <img src={humidity_icon} alt='weather-icon'/>
         <div>
-          <p>91%</p>
+          <p>{weatherData.humidity}%</p>
           <span>Humidity</span>
         </div>
       </div>
       <div className='col'>
         <img src={wind_icon} alt='weather-icon'/>
         <div>
-          <p>32kmph</p>
+          <p>{weatherData.wind}kmph</p>
           <span>Wind Speed</span>
         </div>
       </div>
-
       </div>
+
     </div>
   )
 }
